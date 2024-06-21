331951bfd842822c6ed97383d97e9eb2dcb17111
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-06-21 11:13:25 -0300

    Componente SectionBanner

--- oticas-app/src/components/SectionBanner/index.jsx
+++ oticas-app/src/components/SectionBanner/index.jsx
@@ -5,10 +5,12 @@ import style from "./index.module.css";
 export function SectionBanner() {
   return (
     <section id={style.container_banner}>
-      <div>
-        <p></p>
-        <h1></h1>
-        <p></p>
+      <div className={style.largura_max}>
+        <div className={style.container_banner}>
+          <p>Preço baixos em </p>
+          <h1>Óculos de grau e de sol </h1>
+          <p>Você só encontra aqui</p>
+        </div>
       </div>
     </section>
   );
