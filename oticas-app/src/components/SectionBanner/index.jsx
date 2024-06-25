27e1d5c2cb871463f6f0081b6b9a0886e6adcc15
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-06-25 10:26:41 -0300

    Add medias querys

--- oticas-app/src/components/SectionBanner/index.jsx
+++ oticas-app/src/components/SectionBanner/index.jsx
@@ -2,9 +2,12 @@ import style from "./index.module.css";
 
 export function SectionBanner() {
   return (
-    <section id={style.container_banner}>
+    <section
+      id={style.container_banner}
+      className="d-flex justify-content-center align-items-center"
+    >
       <div className={style.largura_max}>
-        <div className={style.container_banner}>
+        <div className={`d-flex flex-column gap-4 ${style.container_banner}`}>
           <p>Preço baixos em </p>
           <h1>Óculos de grau e de sol </h1>
           <p>Você só encontra aqui</p>
