95555f07c5bbc52eea4b0110989ca7b23bd496fe
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-06-24 21:23:39 -0300

    Refactor styles - Add Bootstrap

--- oticas-app/src/components/Header/index.jsx
+++ oticas-app/src/components/Header/index.jsx
@@ -5,12 +5,42 @@ import style from "./index.module.css";
 
 export function Header() {
   return (
-    <header id={style.cabecalho}>
+    <header
+      id={style.cabecalho}
+      className="
+        mx-auto 
+        py-2 px-3 
+        d-flex 
+          flex-wrap justify-content-between align-items-center"
+    >
       <Image src={Logo} alt="Logo óticas vida" className={style.img_log} />
-      <nav className={style.nav_header}>
-        <Link href="#">PRODUTO</Link>
-        <Link href="#">SOBRE</Link>
-        <Link href="#">CONTATO</Link>
+      <nav className={`${style.nav_header} d-flex gap-3`}>
+        <Link
+          href="#"
+          className="
+             link-secondary link-offset-2 link-underline-opacity-10 link-underline-opacity-50-hover
+            "
+        >
+          PRODUTO
+        </Link>
+        <Link
+          href="#"
+          className="
+          link-secondary link-offset-2 link-underline-opacity-10 
+          link-underline-opacity-50-hover
+          "
+        >
+          SOBRE
+        </Link>
+        <Link
+          href="#"
+          className="
+          link-secondary link-offset-2 link-underline-opacity-10 
+          link-underline-opacity-50-hover
+          "
+        >
+          CONTATO
+        </Link>
       </nav>
     </header>
   );
